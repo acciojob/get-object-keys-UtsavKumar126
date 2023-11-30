d98344fcb5977b563c36f56940bc68b6d8981b3d
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-11-30 20:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,17 +2,12 @@
 function  Student(name){
 	this.name=name;
 }
-Student.prototype.getKeys()= function(){
-	    let keys = [];
-    
-    // Iterate over the object properties and push keys to the 'keys' array
-    for (let key in this) {
-      // Check if the property is directly on the object (not in the prototype chain)
-      if (this.hasOwnProperty(key)) {
-        keys.push(key);
-      }
-    }
-    
-    return keys;
-  };
+Student.prototype.getKeys= function(){
+	let ans=[];
+	for(let i in this){
+	if (this.hasOwnProperty(i)) {
+		ans.push(i);
+	}
+}
+	return ans;
 }
